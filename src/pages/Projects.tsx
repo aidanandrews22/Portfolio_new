5fab2da4e15ac2bcec7d33743b392b99f28f69dc
Aidan Andrews <aidansa2@illinois.edu>
2025-01-28 19:58:05 -0600

    fixed ts error

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,6 +1,5 @@
 import { motion } from 'framer-motion';
 import { useState, useEffect, useMemo } from 'react';
-import { Link } from 'react-router-dom';
 import FilterDropdown from '../components/FilterDropdown';
 
 interface Project {
